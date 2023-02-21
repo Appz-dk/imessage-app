427233685bb865446fccb9db12325e91bfc5318f
Appz-dk <patrick.fyhn@gmail.com>
2023-02-21 11:31:43 +0100

    Setup next auth and google provider

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,16 @@
 import { Inter } from "@next/font/google";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello world</div>;
+  const { data: session, status } = useSession();
+
+  console.log("session data", session);
+  return (
+    <div>
+      {!session?.user && <button onClick={() => signIn("google")}>Sign In</button>}
+      {session?.user && <button onClick={() => signOut()}>Sign Out</button>}
+    </div>
+  );
 }
